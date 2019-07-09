f150d5036e9afaefca178ef7f339cd23d3b4beab
LittleLittleCloud <g2260578356@gmail.com>
2019-07-09 22:26:05 +0000

    add BurgerCoin struct

---
+++ src/struct/BurgerCoin.go
@@ -0,0 +1,21 @@
+package burgercoin
+
+type meta struct {
+	TimeStamp int32
+	DataPad [1024]byte
+}
+
+type certification struct {
+	Pub [1024]byte
+
+	// signiture of last block
+	SigLast [256]byte
+	// signiture of this block
+	SigThis [256]byte
+}
+type BurgerCoin struct {
+	Burble [256]byte
+	UserID [128]byte
+	Meta meta
+	Certification certification
+}
